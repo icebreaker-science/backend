c015ae0f55d0b919617114fd5a405462c856421f
Michael Youssef <michael.kamel.2020@gmail.com>
2020-10-18 22:06:22 +0200

    add response code to accountnotfoundexception

--- src/main/java/science/icebreaker/exception/AccountNotFoundException.java
+++ src/main/java/science/icebreaker/exception/AccountNotFoundException.java
@@ -1,8 +1,12 @@
 package science.icebreaker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * This exception will be thrown if an account does not exist.
  */
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
 public class AccountNotFoundException extends BaseException {
 
 }
